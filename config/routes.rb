cb423a98f6d8428c59ac3a1240191cad731404ab
Andres Santana <aionclerictiamat@gmail.com>
2020-09-27 21:34:23 -0500

    add routes for devise and cart resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
-  root 'movies#index'
+  get 'carts/show'
+
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :movies, only: [:show, :index]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :cart, only: [:show] do
+    put 'add/:movie_id', to: 'carts#add', as: :add_to
+    put 'remove/:movie_id', to: 'carts#remove', as: :remove_from
+  end
+  root 'movies#index'
 end
