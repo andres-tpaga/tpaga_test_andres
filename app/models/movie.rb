8de8f2c8e098712cbc31978f628988f3f8c9e657
Andres Santana <aionclerictiamat@gmail.com>
2020-09-27 21:35:38 -0500

    Add function for cart purposes

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,17 @@
-class Movie < ApplicationRecord
+class Movie < ActiveRecord::Base
+  def poster
+    "https://m.media-amazon.com/images/M/#{poster_url}"
+  end
+
+  def imdb
+    "http://www.imdb.com/title/#{imdb_id}/"
+  end
+
+  def cart_action(current_user_id)
+    if $redis.sismember "cart#{current_user_id}", id
+      "Remove from"
+    else
+      "Add to"
+    end
+  end
 end
